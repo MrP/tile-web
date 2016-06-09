b3e8aefa82f0feddf7a532a8f287673f599677a1
Victor Rodriguez Gil <mrpatan@gmail.com>
2016-06-09 07:19:39 +0000

    fit

--- site/files/victorDragon.js
+++ site/files/victorDragon.js
@@ -1,13 +1,15 @@
+/*global OpenSeadragon, ORIGINAL_PAGE_DIMENSIONS, FILES_DIR*/
 var viewer = OpenSeadragon({
     id: 'openseadragon1',
     prefixUrl: FILES_DIR + '/openseadragon/images/',
     // immediateRender: false,
-    // defaultZoomLevel: 1,
-    // minZoomLevel: null,
-    // maxZoomLevel: null,
-    // homeFillsViewer: true,
-    // constrainDuringPan: true,
+    defaultZoomLevel: 1,
+    minZoomLevel: 0.25,
+    maxZoomLevel: 2,
+    homeFillsViewer: true,
+    constrainDuringPan: true,
     visibilityRatio: 1,
+    // springStiffness: 9.5,
     // gestureSettingsMouse: {
     //     scrollToZoom: false,
     //     // flickEnabled: true
@@ -28,8 +30,13 @@ var viewer = OpenSeadragon({
         tileSize: 256,
         minLevel: 0,
         maxLevel: 8,
-        getTileUrl: function( level, x, y ){
+        getTileUrl: function(level, x, y){
             return '/' + FILES_DIR + '/pagetiles/tile_' + (8-level) + "_" + x + "_" + y + ".png";
         }
     }
+});
+
+viewer.addHandler('open', function () {
+    viewer.viewport.panTo(new OpenSeadragon.Point(0, 0), true);
+    viewer.viewport.applyConstraints(true);
 });
