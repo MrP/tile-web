dfb564214fdd2aeca469f6b40b5fdc0d8d602dff
Victor Rodriguez Gil <mrpatan@gmail.com>
2017-04-28 21:28:57 +0000

    better actions

--- spec/expected/otterprojects/index-tiled-files/tileWeb.js
+++ spec/expected/otterprojects/index-tiled-files/tileWeb.js
@@ -10,8 +10,8 @@ var viewer = OpenSeadragon({
     },
     showFullPageControl: false,
     showHomeControl: false,
-    showNavigationControl: false,
-    showZoomControl: false,
+    showZoomControl: true,
+    autoHideControls: false,
     visibilityRatio: 0.9,
     springStiffness: 19.5,
     zoomPerSecond: 0.5,
@@ -20,10 +20,6 @@ var viewer = OpenSeadragon({
     minZoomImageRatio: 0.1,
     maxZoomPixelRatio: 16,
     defaultZoomLevel: 1,
-
-    // minZoomLevel: 0.25,
-    // maxZoomLevel: EXTRACTED_METADATA.maxLevel + 2,
-    // minPixelRatio: 0.25,
 
     tileSources:   {
         height: EXTRACTED_METADATA.originalPageDimensions.height,
@@ -55,12 +51,16 @@ viewer.addHandler('canvas-scroll', function (event) {
 });
 
 viewer.addHandler('open', function () {
-    var level = viewer.viewport.imageToViewportZoom(1);
-    viewer.viewport.zoomTo(level, new OpenSeadragon.Point(0, 0), true);
-    viewer.viewport.applyConstraints(true);
-    var bounds = viewer.viewport.getBounds(true);
-    viewer.viewport.panTo(new OpenSeadragon.Point(bounds.width/2, bounds.height/2), true);
-    viewer.viewport.applyConstraints(true);
+    try {
+        var level = viewer.viewport.imageToViewportZoom(1);
+        viewer.viewport.zoomTo(level, new OpenSeadragon.Point(0, 0), true);
+        viewer.viewport.applyConstraints(true);
+        var bounds = viewer.viewport.getBounds(true);
+        viewer.viewport.panTo(new OpenSeadragon.Point(bounds.width/2, bounds.height/2), true);
+        viewer.viewport.applyConstraints(true);
+        hideControls(viewer);
+    } catch (e) {
+    }
 });
 
 var victor_links = EXTRACTED_METADATA.originalPageLinks.map(function (link) {
@@ -70,19 +70,57 @@ var victor_links = EXTRACTED_METADATA.originalPageLinks.map(function (link) {
 });
 
 viewer.addHandler('canvas-click', function (event) {
-    if (!event.quick) {
-        return;
-    }
-    var imagePosition = viewer.viewport.viewerElementToImageCoordinates(event.position);
-    var clicked = victor_links.filter(function (link) {
-        return link.osdImageRect.containsPoint(imagePosition);
-    });
-    if (clicked.length >= 1) {
-        if (clicked[0].href) {
-            window.open(clicked[0].href);
-        } else if (clicked[0].onclick) {
-            eval(clicked[0].onclick);
+    try {
+        if (!event.quick) {
+            return;
         }
+        var imagePosition = viewer.viewport.viewerElementToImageCoordinates(event.position);
+        var clicked = victor_links.filter(function (link) {
+            return link.osdImageRect.containsPoint(imagePosition);
+        });
+        if (clicked.length >= 1) {
+            if (clicked[0].href) {
+                window.open(clicked[0].href);
+            } else if (clicked[0].onclick) {
+                eval(clicked[0].onclick);
+            }
+        }
+    } catch (e) {
     }
 });
+
+function hideControls(viewer) {
+    viewer.buttons.buttons.forEach(function (button) {
+        button.element.style.display = 'none';
+    });
+}
+function showControls(viewer) {
+    viewer.buttons.buttons.forEach(function (button) {
+        button.element.style.display = 'inline-block';
+    });
+}
+
+var isTouch = false;
+var touchDetector = function () {
+    try {
+        isTouch = true;
+        hideControls(viewer);
+    } catch (e) {
+    } finally {
+        window.removeEventListener('touchstart', touchDetector);
+        window.removeEventListener('dblclick', doubleClickDetector);
+    }
+};
+var doubleClickDetector = function () {
+    try {
+        if (!isTouch) {
+            showControls(viewer);
+        }
+    } catch (e) {
+    } finally {
+        window.removeEventListener('dblclick', doubleClickDetector);
+    }
+};
+window.addEventListener('touchstart', touchDetector);
+window.addEventListener('dblclick', doubleClickDetector);
 
